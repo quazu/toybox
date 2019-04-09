c97f05168201d1c5d64cc97da0d88b5d4f0220ee
quazu <toedge@gmail.com>
2019-04-09 11:48:16 +0000

    * Dealing arguments.
      - Read the arguments.
      - Parse the first argument (and handle the error).

--- rotx/src/main.rs
+++ rotx/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 1 {
+        usage();
+    }
+
+    match args[1].parse::<i32>() {
+        Result::Ok(val) => println!("You specified {}", val),
+        Result::Err(e) => println!("Oops. {}", e),
+    }
 }
