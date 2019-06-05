aa9f8d3bdc3e596aefb8c84cfac08b208fe11b8e
quazu <toedge@gmail.com>
2019-06-05 09:48:44 +0000

    * Write the function 'rotate'.
      - ToDo: Expand the shift argument minus.

--- rotx/src/main.rs
+++ rotx/src/main.rs
@@ -1,4 +1,5 @@
 use structopt::StructOpt;
+use std::char;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -10,13 +11,26 @@ struct Opt {
 
 fn main() {
     let args = Opt::from_args();
-    let shift: i32 = args.shift.parse().unwrap(); 
+    let shift: u32 = args.shift.parse().unwrap(); 
     println!("{}", shift);
     println!("{}", args.message);
+    let cv: Vec<char> = args.message.chars().collect();
+    for c in cv {
+        println!("{}", rotate(c, shift));
+    }
+    return ();
 }
 
-fn rotate(_c: char, _n: i32) -> char {
-    panic!("NotImplemented");
+fn rotate(c: char, n: u32) -> char {
+    let f = match c.to_digit(36) {
+        Some(v) => (v - 9 + n) % 26 + 9,
+        None => 0
+    };
+    let r = match char::from_digit(f, 36) {
+        Some(x) => x,
+        None => '?',
+    };
+    return r;
 }
 
 #[cfg(test)]
@@ -31,5 +45,10 @@ mod tests {
     fn rotate_1() {
         let result = rotate ('u', 1);
         assert_eq!(result, 'v');
+    }
+    #[test]
+    fn rotate_last() {
+        let result = rotate ('z', 1);
+        assert_eq!(result, 'a');
     }
 }
